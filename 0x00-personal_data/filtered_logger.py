067a4a7e831dec05abb45877ce21eafe7ec24ef4
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-08-01 03:07:06 +0300

    regex

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    returns the log message obfuscated
+    with filter_datum functino
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """ Returns regex obfuscated log messages """
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
